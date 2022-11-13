3e4b366b2da3a3155b046b0fd21a62ac1d538847
hudsonviana <hudson1206@gmail.com>
2022-11-13 00:30:01 -0300

    template engine - handlebars

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 require('dotenv').config();
 const express = require('express');
-const handlebars = require('express-handlebars');
+const { engine } = require('express-handlebars');
+const { Sequelize } = require('sequelize');
+
 const app = express();
 
 // Config
+    // Definição da porta
+    const port = process.env.PORT || 3000;
+    
     // Template Engine
-        app.engine('handlebars', handlebars({defaultLayout: 'main'}));
-        app.set('view engine', 'handlebars');
+    app.engine('handlebars', engine({defaultLayout: 'main'}));
+    app.set('view engine', 'handlebars');
 
-        
-app.listen(process.env.PORT, () => {
-    console.log(`Servidor rodando na porta ${process.env.PORT}`);
+    // Conexão com o banco de dados
+    const sequelize = new Sequelize(
+        process.env.DB_DATABASE,
+        process.env.DB_USERNAME,
+        process.env.DB_PASSWORD, {
+            host: process.env.DB_HOST,
+            dialect: process.env.DB_CONNECTION
+        }
+    );
+//
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
 });
--- teste.js
+++ teste.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const Sequelize = require('sequelize');
+const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
     process.env.DB_DATABASE,
@@ -10,11 +10,81 @@ const sequelize = new Sequelize(
     }
 );
 
-sequelize.authenticate()
-.then(() => {
-    console.log('Conectado com sucesso ao banco de dados.');
-})
-.catch((error) => {
-    console.log('Falha ao se conectar: ' + error);
+// Criar tabela 'postagens'
+// OBS: os campos id, createdAt e updatedAt são criados automaticamente
+const Postagem = sequelize.define('postagens', {
+    titulo: {
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+    }
 });
 
+// Postagem.create({
+//     titulo: 'Um título qualquer',
+//     conteudo: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus labore optio repudiandae sunt veritatis expedita, possimus qui suscipit esse pariatur neque nobis? Atque perspiciatis impedit modi harum reprehenderit enim nulla.'
+// });
+
+const Usuario = sequelize.define('usuarios', {
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+});
+
+// Usuario.create({
+//     nome: 'Hudson',
+//     sobrenome: 'Andrade',
+//     idade: 37,
+//     email: 'hudson@teste.com',
+// });
+
+
+const Teste = sequelize.define('testes', {
+    id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+    },
+    firstName: {
+        type: Sequelize.STRING,
+    },
+    lastName: {
+        type: Sequelize.STRING,
+    },
+});
+
+
+Teste.sync().then((res) => {
+    Teste.create({
+        firstName: 'Maria',
+        lastName: 'da Silva'
+    }).then((res) => {
+        console.log('Dados inseridos com sucesso!', res.id);
+        sequelize.close();
+        console.log('conexão fechada');
+    });
+});
+
+// Teste.create({
+//     firstName: 'Hudson',
+//     lastName: 'Andrade'
+// }).then((res) => {
+//     console.log('Dados inseridos com sucesso!', res.id);
+//     sequelize.close();
+//     console.log('conexão fechada');
+// });
+
+
+// Rodar as migrations
+// Usuario.sync({force:true});
+// Postagem.sync({force: true});
